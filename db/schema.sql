50dd304e304b25756da6b42fea722a31bf1a1828
Sam26 <samueltjenkin@gmail.com>
2023-05-11 15:05:31 +0930

    complete db folder

---
+++ db/schema.sql
@@ -0,0 +1,45 @@
+-- Local DB only:
+CREATE DATABASE careerhive_app;
+\c careerhive_app
+
+-- Run on local and web service
+CREATE TABLE jobs(
+    id SERIAL PRIMARY KEY,
+    job_title TEXT,
+    salary REAL,
+    job_type TEXT,
+    company TEXT,
+    location TEXT,
+    job_url TEXT,
+    job_description TEXT,
+    date_posted TIMESTAMP
+);
+
+INSERT INTO jobs(job_title, salary, job_type, company, location, job_url, job_description, date_posted)
+VALUES
+    ('Psychiatrist', 72000,'Full-time', 'Charterhouse Recruitment (Australia)', 'Gold Coast City
+    ', 'https://www.jobleads.com/registration/job?campaignId=62&jobId=e5190deb2f742dc6a3516cb078043d547&jmId=999&utm_source=jooble%7Cr-jb%7Cc-au&utm_medium=job-external&utm_campaign=jobs&utm_content=jobmarket-id%3D999%7Clandingpage-id%3D158%7Cjob-id%3D5190deb2f742dc6a3516cb078043d547%7Cjob-workarea%3D05-03%7Cjob-hierarchy%3D04%7Cjob-salary-benchmark%3D04%7Cjob-industry%3D46%7Cjob-location%3Dgold-coast-city%7Cjobtitle%3Dpsychiatrist&jobjmlp=158&ccuid=46062991141', 'Charterhouse Medical are currently looking for a Locum Consultant Psychiatrist for an upcoming urgent locum placement as follows.', CURRENT_TIMESTAMP),
+    ('Lawyer', 72000,'Full-time', 'Charterhouse Recruitment (Australia)', 'Gold Coast City
+    ', 'https://www.jobleads.com/registration/job?campaignId=62&jobId=e5190deb2f742dc6a3516cb078043d547&jmId=999&utm_source=jooble%7Cr-jb%7Cc-au&utm_medium=job-external&utm_campaign=jobs&utm_content=jobmarket-id%3D999%7Clandingpage-id%3D158%7Cjob-id%3D5190deb2f742dc6a3516cb078043d547%7Cjob-workarea%3D05-03%7Cjob-hierarchy%3D04%7Cjob-salary-benchmark%3D04%7Cjob-industry%3D46%7Cjob-location%3Dgold-coast-city%7Cjobtitle%3Dpsychiatrist&jobjmlp=158&ccuid=46062991141', 'Charterhouse Medical are currently looking for a Locum Consultant Psychiatrist for an upcoming urgent locum placement as follows.', CURRENT_TIMESTAMP),
+    ('Dean', 72000,'Full-time', 'Charterhouse Recruitment (Australia)', 'Gold Coast City
+    ', 'https://www.jobleads.com/registration/job?campaignId=62&jobId=e5190deb2f742dc6a3516cb078043d547&jmId=999&utm_source=jooble%7Cr-jb%7Cc-au&utm_medium=job-external&utm_campaign=jobs&utm_content=jobmarket-id%3D999%7Clandingpage-id%3D158%7Cjob-id%3D5190deb2f742dc6a3516cb078043d547%7Cjob-workarea%3D05-03%7Cjob-hierarchy%3D04%7Cjob-salary-benchmark%3D04%7Cjob-industry%3D46%7Cjob-location%3Dgold-coast-city%7Cjobtitle%3Dpsychiatrist&jobjmlp=158&ccuid=46062991141', 'Charterhouse Medical are currently looking for a Locum Consultant Psychiatrist for an upcoming urgent locum placement as follows.', CURRENT_TIMESTAMP),
+    ('GP', 72000,'Full-time', 'Charterhouse Recruitment (Australia)', 'Gold Coast City
+    ', 'https://www.jobleads.com/registration/job?campaignId=62&jobId=e5190deb2f742dc6a3516cb078043d547&jmId=999&utm_source=jooble%7Cr-jb%7Cc-au&utm_medium=job-external&utm_campaign=jobs&utm_content=jobmarket-id%3D999%7Clandingpage-id%3D158%7Cjob-id%3D5190deb2f742dc6a3516cb078043d547%7Cjob-workarea%3D05-03%7Cjob-hierarchy%3D04%7Cjob-salary-benchmark%3D04%7Cjob-industry%3D46%7Cjob-location%3Dgold-coast-city%7Cjobtitle%3Dpsychiatrist&jobjmlp=158&ccuid=46062991141', 'Charterhouse Medical are currently looking for a Locum Consultant Psychiatrist for an upcoming urgent locum placement as follows.', CURRENT_TIMESTAMP),
+    ('Dentist', 72000,'Full-time', 'Charterhouse Recruitment (Australia)', 'Gold Coast City
+    ', 'https://www.jobleads.com/registration/job?campaignId=62&jobId=e5190deb2f742dc6a3516cb078043d547&jmId=999&utm_source=jooble%7Cr-jb%7Cc-au&utm_medium=job-external&utm_campaign=jobs&utm_content=jobmarket-id%3D999%7Clandingpage-id%3D158%7Cjob-id%3D5190deb2f742dc6a3516cb078043d547%7Cjob-workarea%3D05-03%7Cjob-hierarchy%3D04%7Cjob-salary-benchmark%3D04%7Cjob-industry%3D46%7Cjob-location%3Dgold-coast-city%7Cjobtitle%3Dpsychiatrist&jobjmlp=158&ccuid=46062991141', 'Charterhouse Medical are currently looking for a Locum Consultant Psychiatrist for an upcoming urgent locum placement as follows.', CURRENT_TIMESTAMP),
+    ('Lecturer', 72000,'Full-time', 'Charterhouse Recruitment (Australia)', 'Gold Coast City
+    ', 'https://www.jobleads.com/registration/job?campaignId=62&jobId=e5190deb2f742dc6a3516cb078043d547&jmId=999&utm_source=jooble%7Cr-jb%7Cc-au&utm_medium=job-external&utm_campaign=jobs&utm_content=jobmarket-id%3D999%7Clandingpage-id%3D158%7Cjob-id%3D5190deb2f742dc6a3516cb078043d547%7Cjob-workarea%3D05-03%7Cjob-hierarchy%3D04%7Cjob-salary-benchmark%3D04%7Cjob-industry%3D46%7Cjob-location%3Dgold-coast-city%7Cjobtitle%3Dpsychiatrist&jobjmlp=158&ccuid=46062991141', 'Charterhouse Medical are currently looking for a Locum Consultant Psychiatrist for an upcoming urgent locum placement as follows.', CURRENT_TIMESTAMP);
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    password_digest TEXT
+);
+
+CREATE TABLE likes(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    job_id INTEGER
+);
