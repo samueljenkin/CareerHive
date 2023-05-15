9ce6a4ac7357c1c1bffb07ddb03905d3e990e5ff
Sam26 <samueltjenkin@gmail.com>
2023-05-15 14:57:47 +0930

    multiple bug fixes and naming fixes

--- models/job.py
+++ models/job.py
@@ -5,20 +5,20 @@ def populate_db():
     api_url = 'https://apis.camillerakoto.fr/fakejobs/jobs'
     response = requests.get(api_url).json()
     for job in response:
-        sql("INSERT INTO jobs(logo, title, salary, name, fulltime, city, zipcode, country, author, content, date) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, CURRENT_TIMESTAMP) RETURNING *", [job['logo'], job['title'], job['salary'], job['name'], job['fulltime'], job['city'], job['zipcode'], job['country'], job['author'], job['content']])
+        sql("INSERT INTO jobs(logo, title, salary, company, employment_type, city, zipcode, country, contact, content, date) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, CURRENT_TIMESTAMP) RETURNING *", [job['logo'], job['title'], 50000, job['name'], job['fulltime'], job['city'], job['zipcode'], job['country'], job['author'], job['content']])
 
 def all_jobs():
-    return sql("SELECT * FROM jobs ORDER BY date DESC")
+    return sql("SELECT * FROM jobs ORDER BY id DESC")
 
-def create_job(user_id, logo, title, salary, name, fulltime, city, zipcode, country, author, content):
-    sql("INSERT INTO jobs(user_id, logo, title, salary, name, fulltime, city, zipcode, country, author, content, date) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, CURRENT_TIMESTAMP) RETURNING *", [user_id, logo, title, salary, name, fulltime, city, zipcode, country, author, content])
+def create_job(user_id, logo, title, salary, company, employment_type, city, zipcode, country, contact, content):
+    sql("INSERT INTO jobs(user_id, logo, title, salary, company, employment_type, city, zipcode, country, contact, content, date) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, CURRENT_TIMESTAMP) RETURNING *", [user_id, logo, title, salary, company, employment_type, city, zipcode, country, contact, content])
 
 def get_job(id):
     jobs = sql("SELECT * FROM jobs WHERE id=%s", [id])
     return jobs[0]
 
-def update_job(logo, title, salary, name, fulltime, city, zipcode, country, author, content, id):
-    sql("UPDATE jobs SET logo=%s, title=%s, salary=%s, name=%s, fulltime=%s, city=%s, zipcode=%s, country=%s, author=%s, content=%s WHERE id=%s RETURNING *", [logo, title, salary, name, fulltime, city, zipcode, country, author, content, id])
+def update_job(logo, title, salary, company, employment_type, city, zipcode, country, contact, content, id):
+    sql("UPDATE jobs SET logo=%s, title=%s, salary=%s, company=%s, employment_type=%s, city=%s, zipcode=%s, country=%s, contact=%s, content=%s WHERE id=%s RETURNING *", [logo, title, salary, company, employment_type, city, zipcode, country, contact, content, id])
 
 def delete_job(id):
     sql("DELETE FROM jobs WHERE id=%s RETURNING *", [id])
@@ -36,7 +36,7 @@ def view_job(job_id, user_id):
     sql("INSERT INTO viewed(job_id, user_id) VALUES(%s, %s) RETURNING *", [job_id, user_id])
 
 def search_jobs(searched):
-    jobs = sql("SELECT * FROM jobs WHERE title LIKE %s OR name LIKE %s OR city LIKE %s OR country LIKE %s", ['%' + searched + '%'] * 4)
+    jobs = sql("SELECT * FROM jobs WHERE title LIKE %s OR company LIKE %s OR city LIKE %s OR country LIKE %s", ['%' + searched + '%'] * 4)
     return jobs
 
 def applied_jobs(user_id):
